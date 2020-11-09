f7bc0cafd5e9e002d7a5abcf350076f03c8aa805
iceberg-1337 <70839369+iceberg-1337@users.noreply.github.com>
2020-11-09 16:16:10 +0300

    Add files via upload

---
+++ Test/src/authors/TestAuthor.java
@@ -0,0 +1,10 @@
+package authors;
+
+import balls.Ball;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+    Author A1 = new Author("Petr","Petr@mail.ru", 'M' );
+    System.out.println(A1);
+    }
+}
