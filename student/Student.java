feadb6e6eb5cfea60b79a7644b1235fa0f378ba3
iceberg-1337 <70839369+iceberg-1337@users.noreply.github.com>
2020-11-09 15:43:37 +0300

    Add files via upload

---
+++ student/Student.java
@@ -0,0 +1,25 @@
+package student;
+
+public class Student implements ExtComparable {
+    int age;
+    String group;
+    public Student(int age, String group) {
+        this.age = age;
+        this.group = group;
+    }
+    public void PrintInfo() {
+        System.out.println("Student: " + group + " " + age);
+    }
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public int compareTo(Object b) {
+        if(b instanceof Student) {
+            Student a = (Student)b;
+            return a.getAge() - this.age;
+        } else {
+            return 0;
+        }
+    }
+}
